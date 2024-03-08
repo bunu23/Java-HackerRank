3456153a0e0ee28a01adfa503d1adee7687fca60
Bunu <bunubhattarai39@gmail.com>
2024-03-07 20:36:35 -0500

    java

---
+++ stdin-stdout/Solution.java
@@ -0,0 +1,22 @@
+package hackerrank;
+import java.util.*;
+public class Solution {
+
+    public static void main(String[] args) {
+     
+    	
+    	Scanner scan=new Scanner(System.in);
+    	
+    	int a=scan.nextInt();
+    	//complete this line
+    	int b=scan.nextInt();
+    	//complete this line
+    	int c=scan.nextInt();
+    	
+    	System.out.println(a);
+    	//complete this line
+    	System.out.println(b);
+    	//complete this line
+    	System.out.println(c);
+    }
+}
